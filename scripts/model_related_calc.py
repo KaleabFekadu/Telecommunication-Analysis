e6c4bbe6088b96d9cf2b7de52c1065b8eaed95a3
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2024-12-20 21:12:28 +0300

    fixed commit

--- scripts/model_related_calc.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def compute_distance(df, centroid,features):
-    return np.sqrt(((df[features] - centroid) ** 2).sum(axis=1))
--- scripts/space.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def giveSpace():
-    for _ in range(5):
-        st.write("")    
